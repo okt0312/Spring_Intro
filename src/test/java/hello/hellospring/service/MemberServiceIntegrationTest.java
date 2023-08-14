9d703b361df9a8f389a1a7a5e706fd6c10ec455c
okt0312 <okt0312@naver.com>
2023-08-15 03:16:12 +0900

    JPA

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,10 +23,11 @@ class MemberServiceIntegrationTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Commit
     public void 회원가입() throws Exception {
         //Given
         Member member = new Member();
-        member.setName("spring3");
+        member.setName("spring100");
         //When
         Long saveId = memberService.join(member);
         //Then
